b264673bbb1dcaa5c1bd2cce64fbb051f911f97c
Maggie Thompson <maggieplayscello@gmail.com>
2020-04-06 14:29:30 -0400

    pushing homepage

---
+++ mvc_java/src/main/java/com/techelevator/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.techelevator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(path="/")
+	public String displayHomePage() {
+		return "Home";
+	}
+	
+	
+}
+	
