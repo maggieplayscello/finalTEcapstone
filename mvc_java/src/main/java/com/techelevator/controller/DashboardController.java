0f483ac1105304c04dfaa80a73510ecfaf0fc51f
Norm Rothwell <normrothwell@gmail.com>
2020-04-15 17:24:51 -0400

    pushing duplicate tee time function

--- mvc_java/src/main/java/com/techelevator/controller/DashboardController.java
+++ mvc_java/src/main/java/com/techelevator/controller/DashboardController.java
@@ -137,14 +137,25 @@ public class DashboardController {
 		List <LocalDateTime> availableTimes = teeTimeDao.getTeeTimesByCourse(course, date);
 		session.setAttribute("availableTimes", availableTimes);
 		session.setAttribute("course", course);
+		session.setAttribute("date", date);
 		return "redirect:/users/{currentUser}/teeTimeSheet";
 	}
 	
 	
 	@RequestMapping (path = "/users/{currentUser}/teeTimeSheet", method = RequestMethod.GET)
-	public String displayTeeTimeSheet(@PathVariable("currentUser") String currentUser, HttpSession session){
+	public String displayTeeTimeSheet(@PathVariable("currentUser") String currentUser, HttpSession session, ModelMap map){
 		session.getAttribute("availableTimes");
 		session.getAttribute("course");
+		session.getAttribute("date");
+		int playerId = userDao.getIdByUserName(currentUser);
+		List <TeeTime> userBookings = teeTimeDao.getTeeTimesByGolferIdPastToday(playerId);
+		List <String> bookings = new ArrayList<>();
+		for (int x=0; x<userBookings.size();x++) {
+			bookings.add(userBookings.get(x).getTime().toLocalDate().toString());
+		}
+		map.put("bookings", bookings);
+		
+		
 		return "teeTimeSheet";
 	}
 	
